8c250d20a391a10fd4db3cf926d4948815419406
Carmelico92 <130740279+Carmelico92@users.noreply.github.com>
2024-04-24 10:55:10 +0000

    terminada tabla de favoritos

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -8,6 +9,9 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites_characters = db.relationship('FavoriteCharacter', backref='users', lazy=True)
+    favorites_planets = db.relationship('FavoritePlanet', backref='users', lazy=True)
+    favorites_vehicles = db.relationship('FavoriteVehicle', backref='users', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -27,6 +31,7 @@ class Characters(db.Model):
     eye_color = db.Column(db.String(250),unique=False, nullable=False)
     gender = db.Column(db.String(250),unique=False, nullable=False)
     hair_color = db.Column(db.String(250),unique=False, nullable=False)
+    favorites_characters = db.relationship('FavoriteCharacter', backref='characters', lazy=True)
 
     def __repr__(self):
         return '<Characters %r>' % self.name
@@ -48,6 +53,7 @@ class Planets(db.Model):
     diameter = db.Column(db.String(250),unique=False,nullable=False)
     rotation_period = db.Column(db.String(250),unique=False, nullable=False)
     orbital_period = db.Column(db.String(250),unique=False, nullable=False)
+    favorites_planets = db.relationship('FavoritePlanet', backref='planets', lazy=True)
 
     def __repr__(self):
         return '<Planets %r>' % self.name
@@ -69,6 +75,7 @@ class Vehicles(db.Model):
     model = db.Column(db.String(250),unique=False,nullable=False)
     vehicle_class = db.Column(db.String(250),unique=False,nullable=False)
     length = db.Column(db.String(250),unique=False,nullable=False)
+    favorites_vehicles = db.relationship('FavoriteVehicle', backref='vehicles', lazy=True)
 
     def __repr__(self):
         return '<Vehicles %r>' % self.name
@@ -81,5 +88,62 @@ class Vehicles(db.Model):
             "vehicle_class":self.vehicle_class,
             "length":self.length
         }
+    
+# class FavoriteUser(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+#     def __repr__(self):
+#         return '<FavoriteUser %r>' % self.user_id
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "user_id":self.user_id
+#         }
+    
+class FavoriteCharacter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<FavoriteCharacter %r>' % self.character_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character_id":self.character_id
+        }
+    
+class FavoritePlanet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<FavoritePlanet %r>' % self.planet_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_id":self.planet_id
+        }
+    
+class FavoriteVehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<FavoriteVehicles %r>' % self.vehicle_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_id":self.vehicle_id
+        }
+
+
 
 
